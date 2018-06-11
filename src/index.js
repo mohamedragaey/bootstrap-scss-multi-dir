a15ef348528577339a5b60a6dc11dd211a9f5375
Ragaey <mragaey@arkdev.net>
2018-06-11 14:15:16 +0200

    patch

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import './scss/bootstrap.scss'
 
 // import UserList class
 // which will provide API of our plugin
-// import {UserList as defaultExport} from './lib/user-list'
+import {UserList as defaultExport} from './lib/user-list'
 
 // export default UserList class
 // I used `defaultExport` to state that variable name doesn't matter
 
-// export default defaultExport
+export default defaultExport
